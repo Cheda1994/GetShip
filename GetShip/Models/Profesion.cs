67dc76d855d2b21bf17beaa726734c41672ba8e0
DanielC <cheda1994@gmail.com>
2017-01-16 12:14:21 +0200

    Add Profession model and few fields to User(Age,Profession)

---
+++ GetShip/Models/Profesion.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
+
+namespace GetShip.Models
+{
+    public class Profesion
+    {
+        public int ProfesionID { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+    }
+
+    public class ProfessionContext : DbContext
+    {
+        public DbSet<Profesion> Professions { get; set; }
+    }
+}
