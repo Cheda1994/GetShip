69e270b9ac85b800e13e5cde12ff502cb02bdeda
DanielC <cheda1994@gmail.com>
2017-01-21 15:29:17 +0200

    Employee was no add previously. Why? ....Dont know!

---
+++ GetShip/Models/Employee.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace GetShip.Models
+{
+    public class Employee
+    {
+        public string Id { get; set; }
+        public string Name { get; set; }
+        public ICollection<double> Salary { get; set; }
+        public string SecretKey { get; set; }
+        public ApplicationUser ApplicationUser { get; set; }
+    }
+
+    public class EmployeeContext : DbContext
+    {
+        public DbSet<Employee> Employeers { get; set; }
+    }
+}
